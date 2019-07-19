9ad12c36b81bc8d2fc6d272ccaacf6c539d3b23e
ElizabethJJames <ElizabethJWJames@gmail.com>
2019-07-19 10:01:49 -0400

    Updates

---
+++ precache-manifest.af68d0b87d29ae4fb3e228eefaa1dbbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d742e97f64de1d44bd1acdc36abb1cc3",
+    "url": "/img-search/build/index.html/index.html"
+  },
+  {
+    "revision": "b971dfc03e23a6d87164",
+    "url": "/img-search/build/index.html/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/img-search/build/index.html/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b971dfc03e23a6d87164",
+    "url": "/img-search/build/index.html/static/js/main.6f34d22e.chunk.js"
+  },
+  {
+    "revision": "77b88a45c7ed7f6cc213",
+    "url": "/img-search/build/index.html/static/js/runtime~main.6b8b4713.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/img-search/build/index.html/static/media/logo.5d5d9eef.svg"
+  }
+]);
